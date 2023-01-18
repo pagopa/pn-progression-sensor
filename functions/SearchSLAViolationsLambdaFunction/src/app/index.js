3cfb2bd09468835c829b6a37e8184bfe1f2d2a80
Marco Iannaccone <marco.iannaccone@pagopa.it>
2023-01-18 14:54:05 +0100

    PN-2788: base input checks for lambda function

--- functions/SearchSLAViolationsLambdaFunction/src/app/index.js
+++ functions/SearchSLAViolationsLambdaFunction/src/app/index.js
@@ -21,18 +21,61 @@
 const handler = async (event, context, callback) => {
   // temp...
   console.log("event", event);
-  const payload = {
-    date: new Date(),
-    message: "Hello Lambda, SearchSLAViolationsLambdaFunction",
-  };
+  // const payload = {
+  //   date: new Date(),
+  //   message: "Hello Lambda, SearchSLAViolationsLambdaFunction",
+  // };
   return JSON.stringify(payload);
 
+  // basic return payload
+  const payload = {
+    success: true,
+    message: "",
+    data: null,
+  };
+
   // 1. input verification -> go on or 400
-  // ...
+  // event = {
+  //  type,           // required, string
+  //  active,         // required, boolean
+  //  olderThan,      // datetime string
+  //  lastScannedKey, // string
+  // }
+
+  // check required input parameters
+  if (!event.type || !event.active) {
+    payload.success = false;
+    payload.message = "required parameters missing";
+    return payload;
+  }
+  // check input paramters correctness
+  if (
+    typeof type !== "string" ||
+    typeof active !== "boolean" ||
+    !isValidDate(olderThan) ||
+    (!lastScannedKey && typeof lastScannedKey !== "string")
+  ) {
+    payload.success = false;
+    payload.message = "incorrect input parameters";
+    return payload;
+  }
   // 2. DynamoDB query
   // ...
   // 3. prepare and return response
   // ...
 };
+
+// UTILS
+
+/**
+ * checks if a string contains a valid datetime
+ *
+ * @param {string} dateString a string containing a datetime (it must be able to become a valida Date object)
+ * @returns {boolean} returns true if the passed string is a correct datetime
+ */
+function isValidDate(dateString) {
+  const schedule_datetime = new Date(dateString);
+  return !Number.isNaN(schedule_datetime.getTime());
+}
 
 export { handler };
