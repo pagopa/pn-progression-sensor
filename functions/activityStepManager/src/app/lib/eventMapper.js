fe69433855ed3c7da64654118530574fae79502e
Fabrizio.D'Ammassa <fabrizio.dammassa@pagopa.it>
2023-01-23 16:44:40 +0100

    PN-2786: refactoring to improve dedup

--- functions/activityStepManager/src/app/lib/eventMapper.js
+++ functions/activityStepManager/src/app/lib/eventMapper.js
@@ -32,189 +32,93 @@ function extractRecIdsFromTimelineId(timelineElementId){
     return lastToken;
 }
 
+function makeDeleteOp(id, type, event){
+    const op = {
+        type: type,
+        id: id,
+        relatedEntityId: event.dynamodb.NewImage.iun.S,
+        opType: 'DELETE',
+        kinesisSeqNumber: event.kinesisSeqNumber
+    }
+
+    return op
+}
+
+function makeInsertOp(id, type, event, timestampFieldName, alarmDays, alarmExpiration){
+    const alarmTTL = calculateNextDate(event.dynamodb.NewImage[timestampFieldName].S, alarmDays)
+    const slaExpiration = calculateNextDate(event.dynamodb.NewImage[timestampFieldName].S, alarmExpiration)
+    const step_alarmTTL = new Date(alarmTTL).getTime();
+    const op = {
+        type: type,
+        id: id,
+        relatedEntityId: event.dynamodb.NewImage.iun.S,
+        startTimestamp: event.dynamodb.NewImage[timestampFieldName].S,
+        slaExpiration: slaExpiration,
+        step_alarmTTL: step_alarmTTL,
+        alarmTTL: alarmTTL,
+        opType: 'INSERT',
+        kinesisSeqNumber: event.kinesisSeqNumber
+    }
+
+    return op
+}
+
 function mapPayload(event){
     const dynamoDbOps = []
     /* istanbul ignore else */
     if(event.tableName=='pn-Notifications') {
-        const alarmTTL = calculateNextDate(event.dynamodb.NewImage.sentAt.S, 0.5)
-        const slaExpiration = calculateNextDate(event.dynamodb.NewImage.sentAt.S, 1)
-        const step_alarmTTL = new Date(alarmTTL).getTime();
-        const op = {
-            type: 'VALIDATION',
-            id: "00_VALID##"+event.dynamodb.NewImage.iun.S,
-            relatedEntityId: event.dynamodb.NewImage.iun.S,
-            startTimestamp: event.dynamodb.NewImage.sentAt.S,
-            slaExpiration: slaExpiration,
-            step_alarmTTL: step_alarmTTL,
-            alarmTTL: alarmTTL,
-            opType: 'INSERT',
-            kinesisSeqNumber: event.kinesisSeqNumber
-        }
+        const op = makeInsertOp("00_VALID##"+event.dynamodb.NewImage.iun.S, 'VALIDATION', event, 'sentAt', 0.5, 1);
         dynamoDbOps.push(op)
     } else if(event.tableName=='pn-Timelines'){
-        let op, recIdx, alarmTTL, slaExpiration, step_alarmTTL;
+        let op, recIdx;
         const category = event.dynamodb.NewImage.category.S;
         switch(category) {
             case 'REQUEST_ACCEPTED':
-                op = {
-                    type: 'VALIDATION',
-                    id: "00_VALID##"+event.dynamodb.NewImage.iun.S,
-                    opType: 'DELETE',
-                    relatedEntityId: event.dynamodb.NewImage.iun.S,
-                    kinesisSeqNumber: event.kinesisSeqNumber
-                }
+                op = makeDeleteOp("00_VALID##"+event.dynamodb.NewImage.iun.S, 'VALIDATION', event)
                 dynamoDbOps.push(op)
 
                 recIdx = extractRecIdsFromTimelineId(event.dynamodb.NewImage.timelineElementId.S)
-
-                alarmTTL = calculateNextDate(event.dynamodb.NewImage.notificationSentAt.S, 110)
-                slaExpiration = calculateNextDate(event.dynamodb.NewImage.notificationSentAt.S, 120)
-                step_alarmTTL = new Date(alarmTTL).getTime();
-
-                const op1 = {
-                    type: 'REFINEMENT',
-                    id: "01_REFIN##"+event.dynamodb.NewImage.iun.S+'##'+recIdx,
-                    relatedEntityId: event.dynamodb.NewImage.iun.S,
-                    startTimestamp: event.dynamodb.NewImage.notificationSentAt.S,
-                    slaExpiration: slaExpiration,
-                    step_alarmTTL: step_alarmTTL,
-                    alarmTTL: alarmTTL,
-                    opType: 'INSERT',
-                    kinesisSeqNumber: event.kinesisSeqNumber
-                }
-
+                const op1 = makeInsertOp("01_REFIN##"+event.dynamodb.NewImage.iun.S+'##'+recIdx, 'REFINEMENT', event, 'notificationSentAt', 110, 120)
                 dynamoDbOps.push(op1)
                 break;
             case 'REQUEST_REFUSED':
-                op = {
-                    type: 'VALIDATION',
-                    id: "00_VALID##"+event.dynamodb.NewImage.iun.S,
-                    opType: 'DELETE',
-                    relatedEntityId: event.dynamodb.NewImage.iun.S,
-                    kinesisSeqNumber: event.kinesisSeqNumber                    
-                }
+                op = makeDeleteOp("00_VALID##"+event.dynamodb.NewImage.iun.S, 'VALIDATION', event)
                 dynamoDbOps.push(op)
 
                 break;
             case 'REFINEMENT':
-                recIdx = extractRecIdsFromTimelineId(event.dynamodb.NewImage.timelineElementId.S)
-
-                op = {
-                    type: 'REFINEMENT',
-                    id: "01_REFIN##"+event.dynamodb.NewImage.iun.S+'##'+recIdx,
-                    opType: 'DELETE',
-                    relatedEntityId: event.dynamodb.NewImage.iun.S,
-                    kinesisSeqNumber: event.kinesisSeqNumber                    
-                }
-
-                dynamoDbOps.push(op)
-                break;
             case 'NOTIFICATION_VIEWED':
                 recIdx = extractRecIdsFromTimelineId(event.dynamodb.NewImage.timelineElementId.S)
-
-                op = {
-                    type: 'REFINEMENT',
-                    id: "01_REFIN##"+event.dynamodb.NewImage.iun.S+'##'+recIdx,
-                    opType: 'DELETE',
-                    relatedEntityId: event.dynamodb.NewImage.iun.S,
-                    kinesisSeqNumber: event.kinesisSeqNumber                    
-                }
-
+                op = makeDeleteOp("01_REFIN##"+event.dynamodb.NewImage.iun.S+'##'+recIdx, 'REFINEMENT', event)
                 dynamoDbOps.push(op)
                 break;
             case 'SEND_DIGITAL_DOMICILE':
-                alarmTTL = calculateNextDate(event.dynamodb.NewImage.timestamp.S, 2)
-                slaExpiration = calculateNextDate(event.dynamodb.NewImage.timestamp.S, 2)
-                step_alarmTTL = new Date(alarmTTL).getTime();
-
-                op = {
-                    type: 'SEND_PEC',
-                    id: "02_PEC__##"+event.dynamodb.NewImage.timelineElementId.S,
-                    relatedEntityId: event.dynamodb.NewImage.iun.S,
-                    startTimestamp: event.dynamodb.NewImage.timestamp.S,
-                    slaExpiration: slaExpiration,
-                    step_alarmTTL: step_alarmTTL,
-                    alarmTTL: alarmTTL,
-                    opType: 'INSERT',
-                    kinesisSeqNumber: event.kinesisSeqNumber
-                }
-
+                op = makeInsertOp("02_PEC__##"+event.dynamodb.NewImage.timelineElementId.S, 'SEND_PEC', event, 'timestamp', 2, 2)
                 dynamoDbOps.push(op)
                 break;
             case 'SEND_DIGITAL_FEEDBACK':
-                op = {
-                    type: 'SEND_PEC',
-                    id: "02_PEC__##"+event.dynamodb.NewImage.timelineElementId.S,
-                    opType: 'DELETE',
-                    relatedEntityId: event.dynamodb.NewImage.iun.S,
-                    kinesisSeqNumber: event.kinesisSeqNumber                    
-                }
-
+                op = makeDeleteOp("02_PEC__##"+event.dynamodb.NewImage.timelineElementId.S, 'SEND_PEC', event)
                 dynamoDbOps.push(op)
                 break;
             case 'SEND_ANALOG_DOMICILE':
             case 'SEND_SIMPLE_REGISTERED_LETTER':
-                alarmTTL = calculateNextDate(event.dynamodb.NewImage.timestamp.S, 100)
-                slaExpiration = calculateNextDate(event.dynamodb.NewImage.timestamp.S, 100)
-                step_alarmTTL = new Date(alarmTTL).getTime();
-
-                op = {
-                    type: 'SEND_PAPER_AR_890',
-                    id: "03_PAPER##"+event.dynamodb.NewImage.timelineElementId.S,
-                    relatedEntityId: event.dynamodb.NewImage.iun.S,
-                    startTimestamp: event.dynamodb.NewImage.timestamp.S,
-                    slaExpiration: slaExpiration,
-                    step_alarmTTL: step_alarmTTL,
-                    alarmTTL: alarmTTL,
-                    opType: 'INSERT',
-                    kinesisSeqNumber: event.kinesisSeqNumber
-                }
-
+                op = makeInsertOp("03_PAPER##"+event.dynamodb.NewImage.timelineElementId.S, 'SEND_PAPER_AR_890', event, 'timestamp', 100, 100)
                 dynamoDbOps.push(op)
                 break;                            
             case 'SEND_ANALOG_FEEDBACK':
-                op = {
-                    type: 'SEND_PAPER_AR_890',
-                    id: "03_PAPER##"+event.dynamodb.NewImage.timelineElementId.S,
-                    opType: 'DELETE',
-                    relatedEntityId: event.dynamodb.NewImage.iun.S,
-                    kinesisSeqNumber: event.kinesisSeqNumber                    
-                }
-
+                op = makeDeleteOp("03_PAPER##"+event.dynamodb.NewImage.timelineElementId.S, 'SEND_PAPER_AR_890', event)
                 dynamoDbOps.push(op)
                 break;                            
             case 'DIGITAL_FAILURE_WORKFLOW':
                 recIdx = event.dynamodb.NewImage.details.M.recIndex.N
-                alarmTTL = calculateNextDate(event.dynamodb.NewImage.timestamp.S, 2)
-                slaExpiration = calculateNextDate(event.dynamodb.NewImage.timestamp.S, 2)
-                step_alarmTTL = new Date(alarmTTL).getTime();
-
-                op = {
-                    type: 'SEND_AMR',
-                    id: "04_AMR##"+event.dynamodb.NewImage.iun.S+'##'+recIdx,
-                    relatedEntityId: event.dynamodb.NewImage.iun.S,
-                    startTimestamp: event.dynamodb.NewImage.timestamp.S,
-                    slaExpiration: slaExpiration,
-                    step_alarmTTL: step_alarmTTL,
-                    alarmTTL: alarmTTL,
-                    opType: 'INSERT',
-                    kinesisSeqNumber: event.kinesisSeqNumber
-                }
+                op = makeInsertOp("04_AMR##"+event.dynamodb.NewImage.iun.S+'##'+recIdx, 'SEND_AMR', event, 'timestamp', 2, 2)
 
                 dynamoDbOps.push(op)
                 break; 
             case 'SEND_SIMPLE_REGISTERD_LETTER_PROGRESS':
                 if(event.dynamodb.NewImage.registeredLetterCode && event.dynamodb.NewImage.registeredLetterCode.S){
                     recIdx = event.dynamodb.NewImage.details.M.recIndex.N
-    
-                    op = {
-                        type: 'SEND_AMR',
-                        id: "04_AMR##"+event.dynamodb.NewImage.iun.S+'##'+recIdx,                        
-                        opType: 'DELETE',
-                        relatedEntityId: event.dynamodb.NewImage.iun.S,
-                        kinesisSeqNumber: event.kinesisSeqNumber
-                    }
-    
+                    op = makeDeleteOp("04_AMR##"+event.dynamodb.NewImage.iun.S+'##'+recIdx, 'SEND_AMR', event)
                     dynamoDbOps.push(op)
                 }
                 break;                 
