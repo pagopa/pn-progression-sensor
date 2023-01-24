2c451b95a84330840f6c0c3b10d82534efc30aa2
Marco Iannaccone <marco.iannaccone@pagopa.it>
2023-01-24 12:26:53 +0100

    PN-3594: basic infrastructure definition

--- functions/slaViolationChecker/src/app/eventHandler.js
+++ functions/slaViolationChecker/src/app/eventHandler.js
@@ -1,5 +1,6 @@
 const dynamo = require("./lib/dynamoFunctions.js");
 
+// input
 module.exports.eventHandler = async (event) => {
   console.log("event: ", event);
 
@@ -11,6 +12,8 @@ module.exports.eventHandler = async (event) => {
 
   try {
     // ...
+    // 1. get delete event from Kinesis
+    // 2. process if reason is TTL: create an Active SLA Violation
   } catch (error) {
     payload.success = false;
     payload.message = error?.message;
