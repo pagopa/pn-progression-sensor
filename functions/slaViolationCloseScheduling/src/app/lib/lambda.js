8d3f76b84e1a5c3649ddafc91ed51c9ad50e482b
Marco Iannaccone <marco.iannaccone@pagopa.it>
2023-02-03 11:07:51 +0100

    PN-2789: lambda function call fix + type check + microservice dashboard

--- functions/slaViolationCloseScheduling/src/app/lib/lambda.js
+++ functions/slaViolationCloseScheduling/src/app/lib/lambda.js
@@ -1,33 +1,44 @@
 const { LambdaClient, InvokeCommand } = require("@aws-sdk/client-lambda");
-const { fromUtf8 } = require("@aws-sdk/util-utf8-node");
+const { Buffer } = require("node:buffer");
+const { isValidType } = require("./utils");
 
 const client = new LambdaClient({ region: process.env.REGION });
 
 exports.getActiveSLAViolations = async (type, lastScannedKey) => {
+  if (!isValidType(type)) {
+    const message = "wrong type passed: " + type;
+    console.error(message);
+    return { success: false, message: message };
+  }
+
+  const lambdaFunctionPayload = {
+    type: type,
+    active: true,
+    lastScannedKey: lastScannedKey,
+  };
+  if (lastScannedKey) {
+    lambdaFunctionPayload.lastScannedKey = lastScannedKey;
+  }
+
   const payLoad = {
     FunctionName:
       process.env.SEARCH_SLA_VIOLATIONS_FUNCTION_NAME ||
       "pn-searchSLAViolationsLambda",
-    InvocationType: "RequestResponse",
-    Payload: fromUtf8(
-      JSON.stringify({
-        type: type,
-        active: true,
-        lastScannedKey: lastScannedKey,
-      })
-    ),
+    //InvocationType: "RequestResponse", // default: synchronous invocation
+    Payload: Buffer.from(JSON.stringify(lambdaFunctionPayload)), // Uint8Array
   };
   const command = new InvokeCommand(payLoad);
 
   try {
     const response = await client.send(command);
-    console.log("lambda invocation response: ", response);
+    const decodedResponse = JSON.parse(Buffer.from(response.Payload));
 
-    // ...
+    console.log("lambda invocation response: ", decodedResponse);
 
-    return {};
+    return decodedResponse;
   } catch (error) {
-    console.log("error calling lambda function: ", error);
-    return {};
+    const message = "error calling lambda function: " + error;
+    console.error(message);
+    return { success: false, message: message };
   }
 };
---
+++ functions/slaViolationCloseScheduling/src/app/lib/utils.js
@@ -0,0 +1,21 @@
+/**
+ * checks if a string contains a valid type
+ *
+ * @param {string} typeString a string containing step type
+ * @returns {boolean} returns true if the passed string is a correct step type
+ */
+exports.isValidType = (typeString) => {
+  const knownTypes = [
+    "VALIDATION",
+    "REFINEMENT",
+    "SEND_PEC",
+    "SEND_PAPER_AR_890",
+    "SEND_AMR",
+  ];
+
+  if (typeof typeString === "string" && knownTypes.includes(typeString)) {
+    return true;
+  }
+
+  return false;
+};
