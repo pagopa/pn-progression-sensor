ab1eb16c459860ad7ba25ba952ce6bbea00da4ba
Marco Iannaccone <marco.iannaccone@pagopa.it>
2023-06-14 18:10:45 +0200

    PN-6650: base logic for function stop/restore

--- functions/slaViolationCloseScheduling/src/app/eventHandler.js
+++ functions/slaViolationCloseScheduling/src/app/eventHandler.js
@@ -2,8 +2,17 @@ const { getActiveSLAViolations } = require("./lib/lambda");
 const { addActiveSLAToQueue } = require("./lib/sqs");
 const { putMetricDataForType } = require("./lib/metrics");
 
+// global lastScannedKey, kept between invocations when the lambda is
+// invoked again and is hot
+let globalLastScannedKey = null;
+let globalLastScannedKeyType = "";
+
 module.exports.eventHandler = async (event) => {
-  //console.log("event: ", event);
+  // INSERT LOGIC: read max allowed execution time from env
+  // ...
+
+  // determine start time
+  const startTime = new Date().getTime();
 
   // basic return payload
   const payload = {
@@ -26,6 +35,20 @@ module.exports.eventHandler = async (event) => {
   for (const type of types) {
     let lastScannedKey = null;
 
+    // check if we have a global last scanned key,
+    // and if it is for the same type
+    if (globalLastScannedKey != null) {
+      if (globalLastScannedKeyType === type) {
+        lastScannedKey = globalLastScannedKey;
+      } else {
+        // reset and break, passing to next type
+        globalLastScannedKeyType = "";
+
+        continue;
+      }
+    }
+
+    // violations inner loop
     let currentTypeSlaViolations = [];
     do {
       const lambdaResponse = await getActiveSLAViolations(type, lastScannedKey);
@@ -34,6 +57,12 @@ module.exports.eventHandler = async (event) => {
         console.log("problem calling lambda function for type: ", type);
         payload.activeSLASearchFailures++;
         lastScannedKey = null;
+
+        // reset global last scanned key
+        // SEE... should we do it?
+        // ...
+        globalLastScannedKey = null;
+        globalLastScannedKeyType = "";
       } else {
         // success
         console.log(
@@ -45,6 +74,26 @@ module.exports.eventHandler = async (event) => {
           lambdaResponse.results
         ); // an array is added to the array for current type
         lastScannedKey = lambdaResponse.lastScannedKey || null;
+
+        // check elapsed time
+        const currentTime = new Date().getTime();
+        const elapsed = currentTime - startTime; // in milliseconds
+
+        let mustStopBeforeTimeout = false; // INSERT LOGIC: calc time till here and compare to max time
+        // ...
+
+        // if we must stop
+        if (mustStopBeforeTimeout) {
+          // we save the last scanned key, if not null
+          if (lastScannedKey != null) {
+            globalLastScannedKey = lastScannedKey;
+            globalLastScannedKeyType = type;
+          }
+
+          break; // stop the loop
+          // SEE... we must stop both loops!!!! while and for... (but probably we automatically exit with the continue from the external one...)
+          /// ...
+        }
       }
     } while (lastScannedKey != null);
 
